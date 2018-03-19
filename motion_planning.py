05030702fed320ddbd102c025f8b2dbe586ff589
Kyle Yoon <apps.yoon@gmail.com>
2018-03-18 20:40:06 -0500

    Collinearity check for pruning the path

--- motion_planning.py
+++ motion_planning.py
@@ -5,7 +5,7 @@ from enum import Enum, auto
 
 import numpy as np
 
-from planning_utils import a_star, heuristic, create_grid
+from planning_utils import a_star, heuristic, create_grid, prune_path
 from udacidrone import Drone
 from udacidrone.connection import MavlinkConnection
 from udacidrone.messaging import MsgID
@@ -156,10 +156,11 @@ class MotionPlanning(Drone):
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         
         # TODO: prune path to minimize number of waypoints
+        pruned_path = prune_path(path)
         # TODO (if you're feeling ambitious): Try a different approach altogether!
 
         # Convert path to waypoints
-        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
+        waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in pruned_path]
         # Set self.waypoints
         print('Found waypoints: {}'.format(waypoints))
         self.waypoints = waypoints
